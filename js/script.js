f02aac59e08d13b2b0b4f2b0258c913c4505f564
Muhammadbek <galaxylogin@yandex.com>
2022-09-16 19:36:09 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let mob_view = window.matchMedia("(max-width: 444px)");
+if (mob_view.matches)
+{
+  var el = document.getElementById("i");
+  var ele = document.getElementById("im");
+  var elem = document.getElementById("ima");
+  var elm = document.getElementById("image");
+  el.classList.add("img-fluid");
+  ele.classList.add("img-fluid");
+  elem.classList.add("img-fluid");
+  elm.classList.add("img-fluid");
+  
+}
+
